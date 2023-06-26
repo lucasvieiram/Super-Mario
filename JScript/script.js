df9975eddc225ec32c4aa909a3dffeac07b0b852
lucasvieiram <lucasvieira.mr@gmail.com>
2023-06-26 16:05:14 -0300

    jogo pronto

--- JScript/script.js
+++ JScript/script.js
@@ -1,5 +1,14 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+function reloadPageOnEnter() {
+    const handleKeyPress = (event) => {
+      if (event.key === "Enter") {
+        location.reload();
+      }
+    };
+  
+    document.addEventListener("keydown", handleKeyPress);
+  }
 
 const jump = () => {
     mario.classList.add('jump');
@@ -10,6 +19,9 @@ const jump = () => {
 }
 
 const loop =  setInterval(() => {
+
+    console.log('loop')
+
     const piperPosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
 
@@ -22,7 +34,10 @@ const loop =  setInterval(() => {
         mario.style.bottom = '${marioPosition}px';
 
         mario.src ='../img/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50  px'
     }
 }, 10)
 
 document.addEventListener('keydown', jump);
+reloadPageOnEnter();
